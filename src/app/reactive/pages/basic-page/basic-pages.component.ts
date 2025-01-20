a954f4b2fe021150b0983169b0364f5ecad311c3
Pucho <martinezochandarenam@gmail.com>
2025-01-20 12:43:37 +0100

    add validation fields

--- src/app/reactive/pages/basic-page/basic-pages.component.ts
+++ src/app/reactive/pages/basic-page/basic-pages.component.ts
@@ -1,5 +1,4 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
 import {  Component, type OnInit } from '@angular/core';
 
 @Component({
@@ -7,29 +6,55 @@ import {  Component, type OnInit } from '@angular/core';
     templateUrl: './basic-page.component.html',
 })
 export class BasicPagesComponent implements OnInit {
-  constructor( private formb: FormBuilder) {}
 
-  // myForm: FormGroup
-  myForm: any
+  public myForm: FormGroup = new FormGroup({});
 
-  initForm(){
+  constructor( private formb: FormBuilder) {};
+
+  initForm(): void{
     this.myForm = this.formb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       price: [0, [Validators.required, Validators.min(0)]],
       inStorage: [0, [ Validators.required, Validators.min(0)]],
     })
-  }
+  };
+
+  isInvalidField(field: string): boolean | null{
+    if(!this.myForm.controls[field]) return null;
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  };
+
+  getFieldError(field: string): string| null{
+
+    if(!this.myForm.controls[field].errors) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    const keys = Object.keys(errors);
+
+    for (const element of keys) {
+      switch (element) {
+        case 'required':
+          return 'Este campo es requerido';
+
+          case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracters.`;
+
+        default:
+          break;
+      };
+    };
+    return null;
+  };
 
 
-  onSave() {
+  onSave(): void {
     if(this.myForm.invalid) return;
-
-    console.log(this.myForm.value)
-
-    this.myForm.reset({price: 0, inStorage: 0});
-  }
+    const initialValues = {price: 0, inStorage: 0};
+    this.myForm.reset(initialValues);
+  };
 
   ngOnInit(): void {
     this.initForm()
   };
-}
+};
